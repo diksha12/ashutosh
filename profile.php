bd162af3748666452b1f690a3e7621814ee0ce17
diksha12 <diksha32@live.com>
2015-03-03 23:56:34 +0530

    added

---
+++ profile.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ashutosh singh</title>
+	<link rel="stylesheet" type="text/css" href="other.css">
+	<link rel="stylesheet" type="text/css" href="reset.css">
+	<link href='http://fonts.googleapis.com/css?family=Dancing+Script|Pacifico|Courgette|Covered+By+Your+Grace|Satisfy|Permanent+Marker|Pinyon+Script' rel='stylesheet' type='text/css'>
+<style type="text/css">
+.outer_box
+{
+	width: 102%;
+	height: 900px;
+	background-color: #000;
+	margin-top: -8px;
+	margin-left: -8px;
+	background-image: url(./ashu.jpg);
+	background-repeat: no-repeat;
+	background-size: 1348px;
+	display: inline-block;
+}
+.top_box
+{
+	width: 102%;
+	height: 128px;
+	background-color: #000;
+	margin-top: -898px;
+	margin-left: -5px;
+}
+.name
+{
+	width: 400px;
+	height: 100px;
+	font-family: Pinyon Script;
+	font-size: 63px;
+	padding-left: 21px;
+	padding-top: 29px;
+	color: bisque;
+	display: inline-block;
+	
+}
+.profile
+{
+	height: 50px;
+	width: 142px;
+	border: 0px;
+	border-radius: 20px;
+	font-size: 30px;
+	font-family: Permanent Marker;
+	color: #2C3E50;
+	cursor: pointer;
+	margin-left: 20px;
+}
+.music
+{
+	height: 50px;
+width: 142px;
+border: 0px none;
+border-radius: 20px;
+font-size: 30px;
+font-family: Permanent Marker;
+color: #2C3E50;
+cursor: pointer;
+margin-left: 20px;
+}
+.abc
+{
+	height: 50px;
+width: 142px;
+border: 0px none;
+border-radius: 20px;
+font-size: 30px;
+font-family: Permanent Marker;
+color: #2C3E50;
+cursor: pointer;
+margin-left: 135px;
+}
+.follow_me
+{
+	height: 50px;
+width: 220px;
+border: 0px none;
+border-radius: 20px;
+font-size: 30px;
+font-family: Permanent Marker;
+color: #2C3E50;
+cursor: pointer;
+margin-left: 20px;
+}
+.hide
+{
+	display: none;
+}
+.arrow
+{
+	
+}
+.arrow_left
+{
+	height: 100px;
+	width: 100px;
+	display: inline-block;
+	margin-top: 200px;
+	background-repeat: no-repeat;
+	background-image: url(./arrowleft.png);
+	background-size: 100px;
+	cursor: pointer;
+}
+.arrow_right
+{
+	height: 100px;
+	width: 100px;
+	display: inline-block;
+	margin-left: 1117px;
+	background-repeat: no-repeat;
+	background-image: url(./arrow_right.png);
+	background-size: 100px;
+	cursor: pointer;
+}
+.outer_box2
+{
+	width: 102%;
+	height: 900px;
+	background-color: #000;
+	margin-top: -439px;
+	margin-left: 1352px;
+	background-image: url(./ashu2.jpg);
+	background-repeat: no-repeat;
+	background-size: 1361px;
+	display: inline-block;
+
+}
+.top_box2
+{
+	width: 102%;
+	height: 128px;
+	background-color: #000;
+	margin-top: -898px;
+	margin-left: 1350px;
+}
+.name2
+{
+	width: 400px;
+	height: 100px;
+	font-family: Pinyon Script;
+	font-size: 63px;
+	padding-left: 21px;
+	padding-top: 29px;
+	color: black;
+	display: inline-block;
+}
+</style>
+</head>
+<body>
+<div class="outer_box">
+	
+</div>
+<div class="top_box">
+	<div class="name">
+			Ashutosh Singh
+	</div>
+	<button class="abc">abc</button>
+	<button class="profile">Profile</button>
+	<button class="music">Music</button>
+	<button class="follow_me">Follow Me</button>
+		
+	</div>
+<div class="arrow">
+	<div class="arrow_left hide"></div>
+	<div class="arrow_right"></div>
+</div>
+<div class="outer_box2"></div>
+<div class="top_box2">
+	<div class="name2">
+			Ashutosh Singh
+	</div>
+	<button class="abc">abc</button>
+	<button class="profile">Profile</button>
+	<button class="music">Music</button>
+	<button class="follow_me">Follow Me</button>
+		
+	</div>
+</body>
+</html>apoorvd31
