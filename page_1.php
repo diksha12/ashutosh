f5ea5a50df8c5acef4ab4d24293518c27d32faec
diksha12 <diksha32@live.com>
2015-03-06 00:42:40 +0530

    2 pages

---
+++ page_1.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html>
+<title>Know Me!!</title>
+<head>
+<link rel="stylesheet" type="text/css" href="reset.css">
+<link rel="stylesheet" type="text/css" href="other.css">
+<link href='http://fonts.googleapis.com/css?family=Courgette|Playball' rel='stylesheet' type='text/css'>
+<style type="text/css">
+.outer_box
+{
+	width: 100%;
+	height: 655px;
+	background-color: black;
+	background-image: url(./page1.jpg);
+	background-repeat: no-repeat;
+	background-size: 1285px;
+	display: inline-block;
+}
+.top_box
+{
+	width: 100%;
+	height: 100px;
+	background-color: red;
+	margin-top: 100px;
+	opacity: 0.6;
+}
+		.name
+		{
+			height: 100%;
+			width: 430px;
+			display: inline-block;
+			margin-left: -1336px;
+			border-right: solid 2px;
+			rgb(135, 135, 172)er-left: solid 2px;
+			border-color: antiquewhite;
+			font-family: Playball;
+			font-size: 65px;
+			border: 0px;
+			color: whitesmoke;
+		}
+		.know_me
+		{
+			height: 100%;
+			width: 175px;
+			background-color: transparent;
+			margin-left: 637px;
+			display: inline-block;
+			float: left;
+			cursor: pointer;
+			font-family: Sigmar One;
+			font-size: 34px;
+			border: 0px;
+			border-left: solid 1px;
+			border-right: solid 1px;
+			color: whitesmoke;
+		}
+		.Compositions
+		{
+			height: 100%;
+			width: 175px;
+			background-color: transparent;
+			display: inline-block;
+			float: left;
+			cursor: pointer;
+			font-family: Sigmar One;
+			font-size: 20px;
+			border: 0px;
+			border-right: solid 1px;
+			color: whitesmoke;
+		}
+		.Videos
+		{
+			height: 100%;
+			width: 175px;
+			background-color: transparent;
+			display: inline-block;
+			float: left;
+			cursor: pointer;
+			font-family: Sigmar One;
+			font-size: 40px;
+			border: 0px;
+			border-right: solid 1px;
+			color: whitesmoke;
+		}
+		.Get_Involved
+		{
+			height: 100%;
+			width: 175px;
+			background-color: transparent;
+			display: inline;
+			float: left;
+			cursor: pointer;
+			font-family: Sigmar One;
+			font-size: 27px;
+			border: 0px;
+			border-right: solid 1px;
+			color: whitesmoke;
+		}
+		.picture
+		{
+		width: 300px;
+		height: 300px;
+		background-color: #F00;
+		border-radius: 158px;
+		margin-left: 30px;
+		margin-top: 50px;
+		display: inline-block;
+		background-image: url("./know_me.jpg");
+		background-repeat: no-repeat;
+		background-size: 307px;
+		}
+		.about_ashu
+		{
+		width: 600px;
+		height: 400px;
+		background-color: transparent;
+		margin-left: 600px;
+		margin-top: -326px;
+		font-size: 50px;
+		color: whitesmoke;
+		font-family: Courgette;
+		}
+	.fixed
+	{
+		height: 100px;
+		width: 600px;
+		margin-left: 896px;
+		margin-top: -93px;
+		position: fixed;
+		font-size: 52px;
+		padding-left: 25px;
+		padding-top: 15px;
+		color: white;
+		font-family: Courgette;
+	}
+	.fb
+	{
+		height: 50px;
+		width: 50px;
+		margin-top: -30px;
+		margin-left: 300px;
+		border-radius: 30px;
+		background-image: url("./fb.png");
+		background-repeat: no-repeat;
+		background-size: 48px;
+		cursor: pointer;
+	}
+	.youtube
+	{
+		background-image: url("./youtube.png");
+		background-repeat: no-repeat;
+		background-size: 50px;
+	}
+	.Linkedin
+	{
+		background-image: url("./linkedIn.jpg");
+		background-repeat: no-repeat;
+		background-size: 50px;	
+	}
+	.button
+	{
+		height: 50px;
+		width: 50px;
+		margin-top: -30px;
+		margin-left: 50px;
+		border-radius: 30px;
+		cursor: pointer;
+	}
+</style>
+</head>
+<body>
+<div class="outer_box">
+	<div class="top_box">
+		<div class="name">Ashutosh Singh</div>
+		<button class= "know_me">Know Me!!</button>
+		<button class="Compositions">My Compositions</button>
+		<button class="Videos">Videos</button>
+		<button class="Get_Involved">Get Involved</button>
+	</div>
+<div class="picture"></div>
+<div class="about_ashu">
+Ashutosh!!
+</div>
+<button class="fb"></button>
+<button class="button youtube"></button>
+<button class="button Linkedin"></button>
+<button class="button tab2"></button>
+<button class="button tab3"></button>
+<div class="fixed">Ashutosh Singh</div>
+</div>
+</body>
+</html>
---
+++ page_2.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html>
+<title>My Compositions!</title>
+<head>
+<link rel="stylesheet" type="text/css" href="reset.css">
+<link rel="stylesheet" type="text/css" href="other.css">
+<link href='http://fonts.googleapis.com/css?family=Courgette|Playball' rel='stylesheet' type='text/css'>
+<style type="text/css">
+.outer_box
+{
+	width: 100%;
+	height: 1060px;
+	background-color: black;
+	display: inline-block;
+	background-image: url("./page2.jpg");
+	background-repeat: no-repeat;
+	background-size: 1630px;
+}
+		.name
+		{
+			height: 100%;
+			width: 400px;
+			display: inline-block;
+			margin-left: -1336px;
+			border-right: solid 2px;
+			rgb(135, 135, 172)er-left: solid 2px;
+			border-color: antiquewhite;
+			font-family: Playball;
+			font-size: 65px;
+			border: 0px;
+			color: whitesmoke;
+
+		}
+		.know_me
+		{
+			height: 100%;
+			width: 175px;
+			background-color: transparent;
+			margin-left: 637px;
+			display: inline-block;
+			float: left;
+			cursor: pointer;
+			font-family: Sigmar One;
+			font-size: 34px;
+			border: 0px;
+			border-left: solid 1px;
+			border-right: solid 1px;
+			color: whitesmoke;
+		}
+		.Compositions
+		{
+			height: 100%;
+			width: 175px;
+			background-color: transparent;
+			display: inline-block;
+			float: left;
+			cursor: pointer;
+			font-family: Sigmar One;
+			font-size: 20px;
+			border: 0px;
+			border-right: solid 1px;
+			color: whitesmoke;
+		}
+		.Videos
+		{
+			height: 100%;
+			width: 175px;
+			background-color: transparent;
+			display: inline-block;
+			float: left;
+			cursor: pointer;
+			font-family: Sigmar One;
+			font-size: 40px;
+			border: 0px;
+			border-right: solid 1px;
+			color: whitesmoke;
+		}
+		.Get_Involved
+		{
+			height: 100%;
+			width: 175px;
+			background-color: transparent;
+			display: inline;
+			float: left;
+			cursor: pointer;
+			font-family: Sigmar One;
+			font-size: 27px;
+			border: 0px;
+			border-right: solid 1px;
+			color: whitesmoke;
+		}
+		.top_box
+{
+	width: 100%;
+	height: 100px;
+	background-color: red;
+	margin-top: 100px;
+	opacity: 0.6;
+}
+.box
+{
+	width: 200px;
+	height: 200px;
+	background-color: red;
+	opacity: 0.7;
+}
+.div1
+{
+	height: 350px;
+	width: 600px;
+	background-color: blue;
+	margin-left: 20px;
+	display: inline-block;
+	margin-top: 20px;
+	opacity: 0.7;
+}
+.div2
+{
+	height: 350px;
+	width: 600px;
+	background-color: blue;
+	margin-left: 113px;
+	display: inline-block;
+	margin-top: 20px;
+	opacity: 0.7;
+}
+.box2
+{
+	height: 100px;
+	width: 373px;
+	background-color: #FF0;
+	margin-left: 210px;
+	margin-top: -151px;
+	opacity: 0.7;
+
+}
+.box3
+{
+	height: 172px;
+	width: 360px;
+	background-color: #FF0;
+	margin-top: 19px;
+	margin-left: 214px;
+	display: inline-block;
+	opacity: 0.7;
+}
+.fixed
+	{
+		height: 100px;
+		width: 450px;
+		margin-left: 852px;
+		font-size: 52px;
+		padding-left: 25px;
+		padding-top: 15px;
+		color: white;
+		font-family: Courgette;
+	}
+</style>
+</head>
+<body>
+<div class="outer_box">
+	<div class="top_box">
+			<div class="name">Ashutosh Singh</div>
+			<button class= "know_me">Know Me!!</button>
+			<button class="Compositions">My Compositions</button>
+			<button class="Videos">Videos</button>
+			<button class="Get_Involved">Get Involved</button>
+	</div>
+	<div class="div1">
+	<div class="box"></div>
+	<div class="box2"></div>
+	<div class="box3"></div>
+	</div>
+
+	<div class="div2">
+	<div class="box"></div>
+	<div class="box2"></div>
+	<div class="box3"></div>
+	</div>
+
+	<div class="div1">
+	<div class="box"></div>
+	<div class="box2"></div>
+	<div class="box3"></div>
+	</div>
+
+	<div class="div2">
+	<div class="box"></div>
+	<div class="box2"></div>
+	<div class="box3"></div>
+	</div>
+	<div class="fixed">
+Ashutosh singh
+</div>
+</div>
+
+</body>
