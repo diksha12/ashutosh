834a18ebea63ad8df0f2571fe46421dd84face33
diksha12 <diksha32@live.com>
2015-03-06 18:39:44 +0530

    rest of pages done

---
+++ page_3.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html>
+<title>Videos</title>
+<head>
+<link rel="stylesheet" type="text/css" href="reset.css">
+<link rel="stylesheet" type="text/css" href="other.css">
+<link href='http://fonts.googleapis.com/css?family=Courgette|Playball' rel='stylesheet' type='text/css'>
+<style type="text/css">
+.outer_box
+{
+	width: 100%;
+	height: 1200px;
+	background-color: black;
+	background-image: url("./page3.jpg");
+	background-repeat: no-repeat;
+	background-size: 2000px;
+}
+.top_box
+{
+	width: 100%;
+	height: 100px;
+	background-color: red;
+	margin-top: 100px;
+	opacity: 0.6;
+	display: inline-block;
+}
+		.name
+		{
+			height: 100%;
+			width: 430px;
+			display: inline-block;
+			margin-left: -1336px;
+			border-right: solid 2px;
+			rgb(135, 135, 172)er-left: solid 2px;
+			border-color: antiquewhite;
+			font-family: Playball;
+			font-size: 65px;
+			border: 0px;
+			color: whitesmoke;
+		}
+		.know_me
+		{
+			height: 100%;
+			width: 175px;
+			background-color: transparent;
+			margin-left: 637px;
+			display: inline-block;
+			float: left;
+			cursor: pointer;
+			font-family: Sigmar One;
+			font-size: 34px;
+			border: 0px;
+			border-left: solid 1px;
+			border-right: solid 1px;
+			color: whitesmoke;
+		}
+		.Compositions
+		{
+			height: 100%;
+			width: 175px;
+			background-color: transparent;
+			display: inline-block;
+			float: left;
+			cursor: pointer;
+			font-family: Sigmar One;
+			font-size: 20px;
+			border: 0px;
+			border-right: solid 1px;
+			color: whitesmoke;
+		}
+		.Videos
+		{
+			height: 100%;
+			width: 175px;
+			background-color: transparent;
+			display: inline-block;
+			float: left;
+			cursor: pointer;
+			font-family: Sigmar One;
+			font-size: 40px;
+			border: 0px;
+			border-right: solid 1px;
+			color: whitesmoke;
+		}
+		.Get_Involved
+		{
+			height: 100%;
+			width: 175px;
+			background-color: transparent;
+			display: inline;
+			float: left;
+			cursor: pointer;
+			font-family: Sigmar One;
+			font-size: 27px;
+			border: 0px;
+			border-right: solid 1px;
+			color: whitesmoke;
+		}
+		.box1
+		{
+			width: 225px;
+			height: 300px;
+			display: inline-block;
+			margin-left: -3px;
+		}
+		.div1
+		{
+			background-color: red;
+			margin-top: -202px;
+		}
+		.div2
+		{
+			background-color: yellow;
+			margin-top: -342px;
+		}
+		.div3
+		{
+			background-color: red;
+			margin-top: -342px;
+		}
+		.div4
+		{
+			background-color: yellow;
+			margin-top: -342px;	
+		}
+		.div5
+		{
+			background-color: red;
+			margin-top: -342px;	
+		}
+		.div6
+		{
+			background-color: yellow;
+			margin-top: -342px;	
+		}
+		.box2
+		{
+			width: 225px;
+			height: 300px;
+			display: inline-block;
+			margin-top: -3px;
+			margin-left: -3px;
+		}
+		.div7
+		{
+			background-color: yellow;
+		}
+		.div8
+		{
+			background-color: red;
+		}
+		.div9
+		{
+			background-color: yellow;
+		}
+		.div10
+		{
+			background-color: red;
+		}
+		.div11
+		{
+			background-color: yellow;
+		}
+		.div12
+		{
+			background-color: red;
+		}
+		.div13
+		{
+			background-color: red;
+		}
+		.div14
+		{
+			background-color: yellow;
+		}
+		.div15
+		{
+			background-color: red;
+		}
+		.div16
+		{
+			background-color: yellow;
+		}
+		.div17
+		{
+			background-color: red;
+		}
+		.div18
+		{
+			background-color: yellow;
+		}
+		.div19
+		{
+			background-color: yellow;
+		}
+		.div20
+		{
+			background-color: red;
+		}
+		.div21
+		{
+			background-color: yellow;
+		}
+		.div22
+		{
+			background-color: red;
+		}
+		.div23
+		{
+			background-color: yellow;
+		}
+		.div24
+		{
+			background-color: red;
+		}
+		.template1
+		{
+			width: 525px;
+			height: 100px;
+			background-color: transparent;
+			margin-top: 300px;
+			margin-left: 66px;
+			position: fixed;
+			font-size: 90px;
+			font-family: Playball;
+
+		}
+		.template2
+		{
+			width: 525px;
+			height: 100px;
+			background-color: transparent;
+			margin-top: 300px;
+			margin-left: 752px;
+			position: fixed;
+			font-size: 90px;
+			font-family: Playball;
+
+		}
+</style>
+</head>
+
+<body>
+<div class="outer_box">
+	<div class="template1">Recommended</div>
+	<div class="template2"> My Videos</div>
+
+	<div class="top_box">
+		<div class="name">Ashutosh Singh</div>
+		<button class= "know_me">Know Me!!</button>
+		<button class="Compositions">My Compositions</button>
+		<button class="Videos">Videos</button>
+		<button class="Get_Involved">Get Involved</button>
+	</div>
+
+	<div class="div1 box1"></div>
+	<div class="div2 box1"></div>
+	<div class="div3 box1"></div>
+	<div class="div4 box1"></div>
+	<div class="div5 box1"></div>
+	<div class="div6 box1"></div>
+	<div class="div7 box2"></div>
+	<div class="div8 box2"></div>
+	<div class="div9 box2"></div>
+	<div class="div10 box2"></div>
+	<div class="div11 box2"></div>
+	<div class="div12 box2"></div>
+	<div class="div13 box2"></div>
+	<div class="div14 box2"></div>
+	<div class="div15 box2"></div>
+	<div class="div16 box2"></div>
+	<div class="div17 box2"></div>
+	<div class="div18 box2"></div>
+	<div class="div19 box2"></div>
+	<div class="div20 box2"></div>
+	<div class="div21 box2"></div>
+	<div class="div22 box2"></div>
+	<div class="div23 box2"></div>
+	<div class="div24 box2"></div>
+
+</div>
+	</div>
+</body>
---
+++ page_4.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html>
+<title>Get Involved</title>
+<head>
+<link rel="stylesheet" type="text/css" href="reset.css">
+<link rel="stylesheet" type="text/css" href="other.css">
+<link href='http://fonts.googleapis.com/css?family=Courgette|Playball' rel='stylesheet' type='text/css'>
+<style type="text/css">
+.outer_box
+{
+	width: 100%;
+	height: 1200px;
+	background-color: black;
+	background-image: url("./page3.jpg");
+	background-repeat: no-repeat;
+	background-size: 2000px;
+}
+.top_box
+{
+	width: 100%;
+	height: 100px;
+	background-color: red;
+	margin-top: 100px;
+	opacity: 0.6;
+	display: inline-block;
+}
+		.name
+		{
+			height: 100%;
+			width: 430px;
+			display: inline-block;
+			margin-left: -1336px;
+			border-right: solid 2px;
+			rgb(135, 135, 172)er-left: solid 2px;
+			border-color: antiquewhite;
+			font-family: Playball;
+			font-size: 65px;
+			border: 0px;
+			color: whitesmoke;
+		}
+		.know_me
+		{
+			height: 100%;
+			width: 175px;
+			background-color: transparent;
+			margin-left: 637px;
+			display: inline-block;
+			float: left;
+			cursor: pointer;
+			font-family: Sigmar One;
+			font-size: 34px;
+			border: 0px;
+			border-left: solid 1px;
+			border-right: solid 1px;
+			color: whitesmoke;
+		}
+		.Compositions
+		{
+			height: 100%;
+			width: 175px;
+			background-color: transparent;
+			display: inline-block;
+			float: left;
+			cursor: pointer;
+			font-family: Sigmar One;
+			font-size: 20px;
+			border: 0px;
+			border-right: solid 1px;
+			color: whitesmoke;
+		}
+		.Videos
+		{
+			height: 100%;
+			width: 175px;
+			background-color: transparent;
+			display: inline-block;
+			float: left;
+			cursor: pointer;
+			font-family: Sigmar One;
+			font-size: 40px;
+			border: 0px;
+			border-right: solid 1px;
+			color: whitesmoke;
+		}
+		.Get_Involved
+		{
+			height: 100%;
+			width: 175px;
+			background-color: transparent;
+			display: inline;
+			float: left;
+			cursor: pointer;
+			font-family: Sigmar One;
+			font-size: 27px;
+			border: 0px;
+			border-right: solid 1px;
+			color: whitesmoke;
+		}
+		.quote
+		{
+			width: 700px;
+			height: 300px;
+			margin-left: 300px;
+			margin-top: -199px;
+			background-color: transparent;
+			font-family: playball;
+			font-size: 70px;
+		}
+		.half1
+		{
+			width: 100%;
+			height: 100px;
+			background-color: transparent;
+			color: blue;
+		}
+		.half2
+		{
+			width: 100%;
+			height: 100px;
+			background-color: transparent;
+			color: blue;
+			margin-top: 100px;
+			padding-left: 250px;
+		}
+		.form
+		{
+			width: 550px;
+			height: 600px;
+			background-color: transparent;
+			margin-left: 20px;
+			display: inline-block;
+			margin-bottom: -373px;
+			font-family: playball;
+			font-size: 24px;
+			padding-top: 20px;
+			padding-left: 10px;
+			color: whitesmoke;
+		}
+		.pic
+		{
+			width: 200px;
+			height: 200px;
+			margin-left: 20px;
+			display: inline-block;
+			border-radius: 100px
+		}
+		.pic1
+		{
+			background-color: yellow;
+		}
+		.pic2
+		{
+			background-color: white;
+			margin-left: 0px;
+		}
+		.pic3
+		{
+			background-color: red;
+			margin-left: 0px;
+		}
+		.pic4
+		{
+			background-color: red;
+			margin-left: 0px;
+			margin-left: 572px;	
+		}
+		.pic5
+		{
+			background-color: red;
+			margin-left: 0px;
+			
+		}
+		.pic6
+		{
+			background-color: red;
+			margin-left: 0px;
+			
+		}
+		.pic7
+		{
+			background-color: red;
+			width: 150px;
+			height: 150px;
+			margin-bottom: 62px;
+			margin-left: 0px;
+		}
+		.pic8
+		{
+			background-color: red;
+			margin-left: 615px;			
+		}
+		.pic9
+		{
+			background-color: red;
+			margin-left: 0px;
+			height: 150px;
+			width: 150px;			
+		}
+		.pic10
+		{
+			background-color: red;
+			margin-left: 0px;
+		}
+		.fname
+	{
+		width: 150px;
+		height: 30px;
+		border-radius: 3px;
+		margin-left: 111px;
+	}
+	.lname
+	{
+		width: 150px;
+		height: 30px;
+		border-radius: 3px;
+	}
+	.dob
+	{
+		width: 131px;
+		height: 30px;
+		border-radius: 3px;
+		margin-left: 41px;
+	}
+	.submit
+	{
+		width: 161px;
+		height: 40px;
+		background-color: transparent;
+		border-radius: 4px;
+		font-family: Playball;
+		font-size: 21px;
+		margin-left: 122px;
+		margin-top: 20px;
+		cursor: pointer;
+		color: white;
+		border: 0px;
+	}
+	.check_f
+	{
+		margin-left: 177px;
+	}
+		</style>
+	</head>
+	<body>
+<div class="outer_box">
+	<div class="top_box">
+		<div class="name">Ashutosh Singh</div>
+		<button class= "know_me">Know Me!!</button>
+		<button class="Compositions">My Compositions</button>
+		<button class="Videos">Videos</button>
+		<button class="Get_Involved">Get Involved</button>
+	</div>
+	<div class="quote">
+		<div class="half1">I See, you are interested</div>
+		<div class="half2">In me!</div>
+	</div>
+	<div class="form">
+		<form method="post">
+			Name:
+			<input type="text" name="f_name" placeholder="First name" class="fname">
+			<input type="text" name="l_name" placeholder="Last name" class="lname"><br><br>
+			date of Birth :
+			<input type="date" name="dob" class="dob">
+			<br><br>
+			Sex (matters the most) :<br>
+			<input type="radio" class="check_f" value="Female" name="gender">
+			Female
+			<input type="radio" name="gender" class="check_m" value="male">
+			male
+			<br>
+			<input type="submit" value="Create my account" class="submit">
+		</form>
+	</div>
+	<div class="pic1 pic"></div>
+	<div class="pic2 pic"></div>
+	<div class="pic3 pic"></div>
+	<div class="pic4 pic"></div>
+	<div class="pic5 pic"></div>
+	<div class="pic6 pic"></div>
+	<div class="pic7 pic"></div>
+	<div class="pic8 pic"></div>
+	<div class="pic9 pic"></div>
+	<div class="pic10 pic"></div>
+</div>
+	</body>
